9054b121d05f5b1efe9ead4ded80658713500056
Adam Hallett <harddrivecaddy@gmail.com>
2014-12-05 08:40:00 -0600

    adding sapp configuration

--- manifests/server.pp
+++ manifests/server.pp
@@ -6,11 +6,28 @@ define halo::server(
   # in game configuration
   $name              = undef,
   $port              = 2302,
+  $maps              = [],
+
+  # sapp specific
   $max_players       = 16,
   $motd              = "",
   $time_limit        = 30,
   $map_cycle_timeout = 3,
   $rcon_password     = undef,
+  $no_lead           = 1,
+  $ping_kick         = 0,
+  $no_lead           = 1,
+  $antihalofp        = 0,
+  $log               = 1,
+  $lua               = 1,
+  $spawn_protection  = 2,
+  $collect_aliases   = 1,
+  $afk_kick          = 600,
+  $anti_warp         = 0,
+  $lua               = 0,
+  $anti_caps         = 1,
+  $anti_spam         = 1,
+  $save_scores       = 1,
 
   $formatted_name = inline_template("<%= @name.split(' ').join('_') %>"),
 
@@ -26,12 +43,6 @@ define halo::server(
   }
 
   file { "${path}/sapp":
-    ensure  => directory,
-    owner   => $owner,
-    group   => $group,
-  }
-
-  file{ "${path}/maps":
     ensure  => directory,
     owner   => $owner,
     group   => $group,
@@ -72,6 +83,19 @@ define halo::server(
     group   => $group,
     content => template('halo/sapp/init.erb'),
     require => File["${path}/sapp"],
+  }
+
+  file { "${path}/motd.txt":
+    path    => "${path}/motd.txt",
+    owner   => $owner,
+    group   => $group,
+    content => template('halo/motd.erb'),
+  }
+
+  file{ "${path}/maps":
+    ensure  => directory,
+    owner   => $owner,
+    group   => $group,
   }
 
   file { "${path}/maps/bloodgulch.map":
